8e2426da9dfe9b56ab9b1d2286990e749d559ac6
Julien Camus <julien.camus@sonarsource.com>
2025-04-29 15:53:03 +0200

    Trigger python:S2876

--- test/__init__.py
+++ test/__init__.py
@@ -2,3 +2,6 @@ class MyClass(object):
     def __init__(self):
         self.message = 'Hello'
         return self  # Noncompliant: a TypeError will be raised
+
+    def __iter__(self):
+        return None  # Noncompliant: Not a valid iterator
