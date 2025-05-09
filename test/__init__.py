fe18a30a24fea290d94a5e5c721bcae10f80c0be
Julien Camus <julien.camus@sonarsource.com>
2025-05-09 15:30:41 +0200

    Trigger issue

--- test/__init__.py
+++ test/__init__.py
@@ -1,4 +1,5 @@
 class MyClass(object):
     def __init__(self):
         self.message = 'Hello'
+        self.bool = 1 <> 2 # Noncompliant: the operator "<>" is deprecated.
         return self  # Noncompliant: a TypeError will be raised - AGAIN
